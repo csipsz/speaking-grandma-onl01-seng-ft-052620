410387e747a77a22fc7bd0b086c14552a1d00629
Luca Kanyo <lucakanyo@protonmail.com>
2020-05-26 21:35:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,9 @@
 
 def speak_to_grandma(words)
   if words == words.upcase
-    puts ""
+    puts "NO, NOT SINCE 1938!"
+  end 
+  elsif words == 
     
-    HUH?! SPEAK UP, SONNY!
+    "HUH?! SPEAK UP, SONNY!"
 end
